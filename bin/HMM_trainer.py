8cea265792a63ae04c485ff0b02e47bdf6290aa4
Joel <joel.boyd@uqconnect.edu.au>
2015-07-13 10:51:01 +1000

    wirtes more output to a file now

--- bin/HMM_trainer.py
+++ bin/HMM_trainer.py
@@ -1,4 +1,4 @@
-import argparse, os, sys, subprocess, regex, re
+import argparse, os, sys, subprocess, regex, re, logging
 
 PATH_TO_GENOMES = "/srv/db/img/latest/genomes/"
 PATH_TO_BAMS    = "/srv/projects/abisko/annotation/joel/01_training_set/bams/"
@@ -10,7 +10,7 @@ class HMMTrainer:
     
     def _get(self, path):
         for id in [x.strip().split() for x in open(path, 'r').readlines()]:
-            yield id[0], id[1]
+            yield id[0], id[1].split(',')
     
     def _rgff(self, gff_file):
         genes_id_to_region={}
@@ -67,17 +67,22 @@ class HMMTrainer:
         found_read_names_list = [[key, max(item)] for key, item \
                                  in found_read_names_hash.iteritems()]
 
-        return found_read_names_list
-        
+        return found_read_names_list        
+    
     def train(self, hmm, genome_ids):
-        
-        max_known_hits      = []
+        output_path         = hmm.replace('.hmm', '_cutoffs.txt')
+        min_known_hits      = []
         max_false_positives = []
         false_negatives     = []
+        mock_genomes        = [x for x in os.listdir(PATH_TO_MOCKS)]
+        logging.info('Read in mock genomes')
         
-        for gene_id, genome_id in self._get(genome_ids):
+        for genome_id, gene_ids in self._get(genome_ids):
             known_hits      = []
             false_positives = []
+            known_reads     = []
+            seen_genomes    = []
+            
             genome_file = os.path.join(PATH_TO_GENOMES, 
                                        genome_id, 
                                        genome_id + '.fna')
@@ -89,39 +94,73 @@ class HMMTrainer:
             mock_file   = os.path.join(PATH_TO_MOCKS,
                                        genome_id + '_sammy_mock.fna')
             
-            known_reads = set(
-                              self._freadsin(gene_id, 
-                                             genome_file, 
-                                             gff_file, 
-                                             bam_file)
-                              )
-            
-            found_reads = self._srch(
-                                     hmm, 
-                                     mock_file
-                                     )
-
-            kr = [x for x in known_reads]
-            
-            for hit in found_reads:
-                hit_name = hit[0]
-                hit_bit  = hit[1]
+            if os.path.getsize(genome_file) > 0:
                 
-                if hit_name in known_reads:
-                    known_hits.append(hit_bit)
-                    kr.remove(hit_name)
-                else:
-                    false_positives.append(hit_bit)
+                for gene_id in gene_ids:
+                    known_reads += self._freadsin(gene_id, 
+                                                  genome_file, 
+                                                  gff_file, 
+                                                  bam_file)
+                               
+    
+                found_reads = self._srch(
+                                         hmm, 
+                                         mock_file
+                                         )
+                
+                known_reads = set(known_reads)
+                kr = [x for x in known_reads]
+                
+                for hit in found_reads:
+                    hit_name = hit[0]
+                    hit_bit  = hit[1]
+                    
+                    if hit_name in known_reads:
+                        known_hits.append(hit_bit)
+                        kr.remove(hit_name)
+                    else:
+                        false_positives.append(hit_bit)
+    
+                false_negatives = kr
+                
+                if known_hits:
+                    min_known_hits.append(min(known_hits))
+                if false_positives:
+                    max_false_positives.append(max(false_positives))
+                
+                mock_genomes.remove(genome_id+'_sammy_mock.fna')
+        
+        for genome in mock_genomes:
+            genome_path = os.path.join(PATH_TO_MOCKS, genome)
+            if os.path.getsize(genome_path) > 0:
+                result = self._srch(
+                                    hmm,
+                                    genome_path
+                                    )
+                if result:
+                    max_false_positives.append(max([x[1] for x in result]))
 
-            false_negatives = kr
-            
-            if known_hits:
-                max_known_hits.append(min(known_hits))
-            if false_positives:
-                max_false_positives.append(max(false_positives))
-            
-        import IPython ; IPython.embed()
-            
-            
+        
+        TC_average = str(sum(min_known_hits)/len(min_known_hits))
+        NC_average = str(sum(max_false_positives)/len(max_false_positives))
+        TC_max     = str(max(min_known_hits))
+        NC_max     = str(max(max_false_positives))
+        TC_min     = str(min(min_known_hits))
+        NC_min     = str(min(max_false_positives))
+        
+        output     = '''TC_average: %s
+TC_max: %s
+TC_min: %s
+NC_average: %s
+NC_max: %s
+NC_min: %s''' % (TC_average, 
+                 TC_max, 
+                 TC_min, 
+                 NC_average, 
+                 NC_max, 
+                 NC_min)
+        
+        with open(output_path, 'w') as out:
+            out.write(output)
         
 HMMTrainer().train(sys.argv[1], sys.argv[2])
